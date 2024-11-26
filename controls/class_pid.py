d27a9c2ad03f1277f75a4325c858a615f3d398fd
MTE 380 PI <jackylim1301@gmail.com>
2024-11-26 00:09:57 -0500

    update pid code

--- controls/class_pid.py
+++ controls/class_pid.py
@@ -1,28 +1,43 @@
 import time
 
-DEFAULT_KP = 1
-DEFAULT_KD = 1 
+DEFAULT_KP = 0.3
 DEFAULT_KI = 0
+DEFAULT_KD = 0.1
 
-class pid:
+class PID:
     def __init__(self, pGain = DEFAULT_KP, dGain = DEFAULT_KD, iGain = DEFAULT_KI):
         self.kP = pGain
         self.kD = dGain
         self.kI = iGain
         self.previousError = 0
-        self.previousTime = time.clock()
+        self.previousTime = time.perf_counter()
         self.integralBuildUp = 0
+        self.integral_limit = 10.0  # Prevent integral windup (although we probably don't need it)
+    
+    def reset(self):
+        """Reset the PID controller state."""
+        self.previousError = 0.0
+        self.previousTime = time.perf_counter()
+        self.integralBuildUp = 0.0
     
     def computeOutput(self, currentPosition, goal):
-        currTime = time.clock()
+        """Compute the PID output given the current position and goal."""
+        currTime = time.perf_counter()
+        dt = currTime - self.previousTime
+
+        if dt <= 0:  # Prevent division by zero or negative time
+            dt = 1e-6
+
         error = goal - currentPosition
-        
-        derivative = (currentPosition - self.previousError)/(currTime - self.previousTime)
+        derivative = (error - self.previousError) / dt
 
-        self.integralBuildUp += error*(currTime - self.previousTime)
+        self.integralBuildUp += error * dt
+        # Clamp the integral to prevent windup
+        self.integralBuildUp = max(min(self.integralBuildUp, self.integral_limit), -self.integral_limit)
 
+        # Update previous state
         self.previousError = error
         self.previousTime = currTime
 
-        return error*self.kP + derivative*self.kD + self.integralBuildUp*self.kI
-    
+        # Compute PID output
+        return (error * self.kP) + (derivative * self.kD) + (self.integralBuildUp * self.kI)
